fa419f8debf6619f92980d031a77a6ccbd700955
wward <woward00@gmail.com>
2021-10-30 23:13:12 -0600

    classes and files

--- bayesian_network.py
+++ bayesian_network.py
@@ -1,2 +1,9 @@
 class BayesianNetwork:
-    pass
+
+    def __init__(self, bif_file: str) -> None:
+        self.bif_file: str = bif_file
+        pass
+
+    def generate_network_from_bif(self) -> None:
+        pass
+
