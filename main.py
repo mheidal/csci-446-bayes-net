db211d6c16ed91c8a8d8307679b2eb46c656c8d9
wward <woward00@gmail.com>
2021-11-07 10:12:25 -0700

    mods to main

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+from typing import List
+
+from node import Node
+
 from bayesian_network import BayesianNetwork
 
 
 def main() -> None:
-    bayesian_network: BayesianNetwork = BayesianNetwork(bif_file_name="child.bif")
-    # bayesian_network: BayesianNetwork = BayesianNetwork(bif_file_name="hailfinder.bif")
+    # bayesian_network: BayesianNetwork = BayesianNetwork(bif_file_name="child.bif")
+    bayesian_network: BayesianNetwork = BayesianNetwork(bif_file_name="hailfinder.bif")
     print(bayesian_network)
     print(f"Nodes: {len(bayesian_network.get_nodes())}")
-    ordering = bayesian_network.dfs()
+    ordering: List[Node] = bayesian_network.dfs()
     print(ordering)
     print(len(ordering))
-
 
 
 if __name__ == '__main__':
